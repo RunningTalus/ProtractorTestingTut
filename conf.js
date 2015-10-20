4bdccd7685467d4cc2a53c900b4840df5085fb12
Dave May <dave@davemay.org>
2015-10-20 15:40:26 -0600

    Initial commit. Writing a test.

---
+++ conf.js
@@ -0,0 +1,8 @@
+// conf.js
+
+
+exports.config = {
+    framework: 'jasmine2',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js']
+}
---
+++ spec.js
@@ -0,0 +1,10 @@
+// spec.js
+
+
+describe('Protractor Demo App', function() {
+    it('should have a title', function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+});
