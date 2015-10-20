45eda7beae028601c4834c9fb71dd7364f556a1a
Dave May <dave@davemay.org>
2015-10-20 16:00:29 -0600

    Writing multiple scenarios.

--- spec.js
+++ spec.js
@@ -1,6 +1,37 @@
 // spec.js
 
 
+describe('Protractor Demo App', function() {
+    var firstNumber = element(by.model('first'));
+    var secondNumber = element(by.model('second'));
+    var goButton = element(by.id('gobutton'));
+    var latestResult = element(by.binding('latest'));
+
+    beforeEach(function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
+
+    it('should have a title', function() {
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+
+    it('should add one and two', function() {
+        firstNumber.sendKeys(1);
+        secondNumber.sendKeys(2);
+
+        goButton.click();
+
+        expect(latestResult.getText()).toEqual('3');
+    });
+
+    it('should add four and six', function() {
+        firstNumber.sendKeys(4);
+        secondNumber.sendKeys(6);
+        goButton.click();
+        expect(latestResult.getText()).toEqual('10');
+    });
+});
+
 /*
 describe('Protractor Demo App', function() {
     it('should have a title', function() {
@@ -12,6 +43,7 @@ describe('Protractor Demo App', function() {
 */
 
 
+/*
 describe('Protractor Demo App', function() {
     it('should add one and two', function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
@@ -21,6 +53,8 @@ describe('Protractor Demo App', function() {
         element(by.id('gobutton')).click();
 
         expect(element(by.binding('latest')).getText()).
-            toEqual('3');
+            toEqual('5'); // This is wrong!
     });
 });
+*/
+
