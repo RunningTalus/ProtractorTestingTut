56fd67481a533fcc1501c71b373d7e0359b7b86e
Dave May <dave@davemay.org>
2015-10-20 16:02:36 -0600

    Changing the configuration.

--- conf.js
+++ conf.js
@@ -4,5 +4,18 @@
 exports.config = {
     framework: 'jasmine2',
     seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js'],
+    multiCapabilities: [{
+        browserName: 'firefox'
+    }, {
+        browserName: 'chrome'
+    }]
+}
+
+/*
+exports.config = {
+    framework: 'jasmine2',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['spec.js']
 }
+*/
